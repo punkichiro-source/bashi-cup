8ed760a15d4ba6aa660f0fa4cf08885392a0c0ea
punkichiro-source <punkichiro@gmail.com>
2026-06-27 19:57:13 +0900

    Update and rename matches.tsx to matches.index.tsx

--- src/routes/matches.tsx
+++
@@ -1,7 +0,0 @@
-// src/routes/matches.tsx
-import { createFileRoute, Outlet } from "@tanstack/react-router";
-
-// 親ルートを定義
-export const Route = createFileRoute("/matches")({
-  component: () => <Outlet />,
-});
